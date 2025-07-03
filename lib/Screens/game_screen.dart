2b06a0f8a2aeed032f1e96e9e4d1f5f434ea2d49
Mike4A <michael.fink@live.at>
2025-07-03 10:29:12 +0200

    start

---
+++ lib/Screens/game_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/game_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lights out',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const GameScreen(title: 'Lights out'),
+    );
+  }
+}
